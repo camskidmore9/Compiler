c1e9f81dd55e38ed713fc01513d602116aa70243
Cameron <camskidmore2000@gmail.com>
2024-02-06 08:27:57 -0500

    Setting up rust project

---
+++ target/debug/deps/compiler-04f051a1a40ef172.d
@@ -0,0 +1,5 @@
+/home/cam/data/Compiler/target/debug/deps/libcompiler-04f051a1a40ef172.rmeta: src/main.rs
+
+/home/cam/data/Compiler/target/debug/deps/compiler-04f051a1a40ef172.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/compiler-0d37edd287f20dee.d
@@ -0,0 +1,5 @@
+/home/cam/data/Compiler/target/debug/deps/libcompiler-0d37edd287f20dee.rmeta: src/main.rs
+
+/home/cam/data/Compiler/target/debug/deps/compiler-0d37edd287f20dee.d: src/main.rs
+
+src/main.rs:

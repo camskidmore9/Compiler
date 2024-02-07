035a370779f0b334058e877ce6d6dad35f04a0bb
Cameron <camskidmore2000@gmail.com>
2024-02-06 22:37:35 -0500

    broken dumb shit

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,72 @@
-use {
-    anyhow::Result,
-    parse_display::Display,
-    std::{
-        env,
-        fs::File,
-        io::{BufRead, BufReader},
-    },
-    utf8_chars::BufReadCharsExt,
-};
+//Rules
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
 
 extern crate anyhow;
 extern crate parse_display;
 extern crate utf8_chars;
 
+use {
+    anyhow::Result,
+    parse_display::Display,
+    std::{
+        env,
+        fs::{
+            File,
+            read_to_string,
+        },
+        path::Path,
+        io::{
+            Read,
+            prelude::*,
+            BufReader,
+            BufRead
+        },
+
+
+    },
+    utf8_chars::BufReadCharsExt,
+};
+
+//imports
+use std::io::prelude::*;
+
+
 #[derive(Clone, Debug, Display)]
-#[display("{characters} {words} {lines}")]
+#[display("format")]
+
+
+
+//inFile Class
+struct inFile{
+    attatchFile: bool,
+    fileName: String,
+    lineCnt: usize,
+    file : BufReader <File>,
+}
+impl inFile {
+    fn new(fileName: &str) -> inFile {
+        let mut newFile = BufReader::new(File::open(fileName).unwrap());
+        
+        inFile {
+            fileName: fileName.to_string(),
+            attatchFile: false,
+            lineCnt: 0,
+            file: newFile,
+        }
+
+    }
+}
+
+
+//Stats class, breaks down a file and gets the stats from it
 struct Stats {
     characters: usize,
     words: usize,
     lines: usize,
 }
-
 impl Stats {
     fn new<R: BufRead>(mut reader: R) -> Result<Self> {
         let mut stats = Stats {
@@ -54,11 +100,16 @@ impl Stats {
 }
 
 fn main() -> Result<()> {
-    for path in env::args().skip(1) {
-        let file = BufReader::new(File::open(&path)?);
-        let stats = Stats::new(file)?;
-        //println!(" {} {}", stats, path);
-    }
+    
+    let path = env::args().nth(1).expect("please supply an argument");
+    let file = BufReader::new(File::open(&path)?);
+    let stats = Stats::new(file)?;
+    println!("File: {} characters: {}", path, stats.characters);
+
+    println!("Creating inFile structure");
+    let f = inFile::new(path.as_str(), stats.lines);
+    println!("inFile: {}", f.fileName);
+
 
     Ok(())
 }
